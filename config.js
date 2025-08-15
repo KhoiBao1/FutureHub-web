76ca962f4b722aac9af85ec8c434d73196329a36
Bảo Khôi <khoibao928@gmail.com>
2025-08-15 15:00:44 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,12 @@
+// config.js
 require('dotenv').config();
+const mongoose = require('mongoose');
 
-module.exports = {
-  app: {
-    port: process.env.PORT || 3000
-  },
-  database: {
-    connection: process.env.MONGODB_CONNECT_URI,
-    option: {
-      autoIndex: false
-    }
-  },
-  session: {
-    key: '27bda112-99dd-4496-8015-ea20d1034228'
-  }
-};
+mongoose.connect(process.env.MONGODB_CONNECT_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log('MongoDB connected'))
+.catch(err => console.error('MongoDB connection error:', err));
+
+module.exports = mongoose;
