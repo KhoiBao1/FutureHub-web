0256c4ffbbb5966731025cdfb2e62d5e5ae13026
Bảo Khôi <khoibao928@gmail.com>
2025-08-22 15:13:22 +0700

    Update admin.js

--- routes/admin.js
+++ routes/admin.js
@@ -6,6 +6,41 @@ const Passport = require('../modules/passport');
 const ProductModel = require('../models/product');
 const UserModel = require('../models/user');
 
+/**
+ * @swagger
+ * tags:
+ *   name: Admin
+ *   description: Các API quản trị
+ */
+
+/**
+ * @swagger
+ * /admin/getUser:
+ *   post:
+ *     summary: Lấy thông tin user hiện tại
+ *     tags: [Admin]
+ *     responses:
+ *       200:
+ *         description: Trả về thông tin user
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 _id:
+ *                   type: string
+ *                   example: "64f1234abcd56789ef012345"
+ *                 username:
+ *                   type: string
+ *                   example: "admin"
+ *                 email:
+ *                   type: string
+ *                   example: "admin@example.com"
+ */
+router.post('/getUser', Passport.requireAuth, (req, res) => {
+  res.json(req.user);
+});
+
 router.get('/', Passport.requireAuth, async (req, res) => {
   const data = {
     order: 0,
@@ -15,9 +50,7 @@ router.get('/', Passport.requireAuth, async (req, res) => {
   };
 
   data.order = await OrderModel.find().count();
-
   data.product = await ProductModel.find().count();
-
   data.user = await UserModel.find().count();
 
   const arr = await OrderModel.find(
@@ -33,11 +66,6 @@ router.get('/', Passport.requireAuth, async (req, res) => {
   }
 
   res.render('admin/index', data);
-});
-
-
-router.post('/getUser', Passport.requireAuth, (req, res) => {
-  res.json(req.user);
 });
 
 module.exports = router;
