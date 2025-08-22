7290c8a57cf3a613f8bae5f43a294b8593393336
Bảo Khôi <khoibao928@gmail.com>
2025-08-22 15:32:42 +0700

    Update index.js

--- index.js
+++ index.js
@@ -24,9 +24,6 @@ const session = require('express-session');
 
 const app = express();
 
-// --- Thêm static public ---
-app.use(express.static(path.join(__dirname, 'public')));
-
 // --- View engine setup ---
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
